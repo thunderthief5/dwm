85fd9ce78701d5c9d41d5b5aaa60a0278536ac5e
ThunderThief <thunderthief5@gmail.com>
2022-01-28 13:58:31 +0530

    Added autostart.sh and dwmbar scripts

---
+++ autostart.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#function run {
+# if ! pgrep $1 ;
+#  then
+#    $@&
+#  fi
+#}
+picom &
+#sxhkd -c ~/.dwm/sxhkdrc &
+xsetroot -cursor_name left_ptr &
+sleep 1 && nm-applet &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+ffplay -nodisp -autoexit -loglevel quiet ~/.local/share/sounds/Startup-XboxOne.mp3 &
+dunst &
+xfce4-power-manager &
+xfsettingsd &
+nitrogen --restore &
+volumeicon &
+xautolock -time 10 -locker blurlock -corners 00-0 &
+xrdb ~/.Xresources &
+~/.dwm/dwmbar.sh &
+
---
+++ dwmbar.sh
@@ -0,0 +1,175 @@
+#!/usr/bin/env bash
+
+### {{{ Modules
+
+# Volume
+volume (){
+if command -v amixer 2>&1 >/dev/null; then
+    vol_stat=$(amixer sget Master | tail -n1 | sed -r "s/.*\[(.*)\]/\1/")   
+    vol=`amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/'`
+	if [ "$vol_stat" = "off" ]; then
+    		echo "M"
+    	else
+	        echo -e "$vol"	
+	fi        
+elif command -v pamixer 2>&1 >/dev/null; then
+    if [ "$(pamixer --get-mute)" = "true" ]; then
+        echo 'M'
+    else
+        echo $(pamixer --get-volume)
+    fi
+fi
+}
+
+# Capslock
+capslock() {
+  if [ "$(xset -q | sed -n 's/^.*Caps Lock:\s*\(\S*\).*$/\1/p')" = "on" ]; then
+      echo "CAPS"
+    fi
+}
+
+# Screen Brightness
+brightness_value() {
+  cat /sys/class/backlight/intel_backlight/brightness
+}
+brightness() {
+  expr $(brightness_value) / 44
+}
+
+# WiFi
+network() {
+wifi="$(ip a | grep wlan0 | grep inet | wc -l)"
+## wire="$(ip a | grep eth0 | grep inet | wc -l)"
+## wifi="$(ip a | grep wlan | grep inet | wc -l)"
+wire="$(ip a | grep enp1s0 | grep inet | wc -l)"
+if [ $wire = 1 ]; then
+    echo "UP "
+elif [ $wifi = 1 ]; then
+    echo "UP"
+else
+    echo "DOWN"
+fi
+}
+
+# Battery
+bat_per0() {
+    cat /sys/class/power_supply/BAT0/capacity
+}
+bat_stat0() {
+    cat /sys/class/power_supply/BAT0/status
+}
+bat_icon0() {
+    case "$(bat_stat)" in
+        'Charging') echo ''
+        ;;
+        'Not Charging') echo ''
+        ;;
+        'Discharging')
+            case "$(bat_per0)" in
+                '100') echo ''
+                ;;
+                '9'*) echo ''
+                ;;
+                '8'*) echo ''
+                ;;
+                '7'*) echo ''
+                ;;
+                '6'*) echo ''
+                ;;
+                '5'*) echo ''
+                ;;
+                '4'*) echo ''
+                ;;
+                '3'*) echo ''
+                ;;
+                '2'*) echo ''
+                ;;
+                '1'*) echo ''
+                ;;
+                '0'*) echo ''
+                ;;
+                *) echo ''
+                ;;
+
+            esac
+        ;;
+        *) echo ''
+        ;;
+    esac
+
+}
+
+bat_per1() {
+    cat /sys/class/power_supply/BAT1/capacity
+}
+bat_stat1() {
+    cat /sys/class/power_supply/BAT1/status
+}
+bat_icon1() {
+    case "$(bat_stat)" in
+        'Charging') echo ''
+        ;;
+        'Not Charging') echo ''
+        ;;
+        'Discharging')
+            case "$(bat_per1)" in
+                '100') echo ''
+                ;;
+                '9'*) echo ''
+                ;;
+                '8'*) echo ''
+                ;;
+                '7'*) echo ''
+                ;;
+                '6'*) echo ''
+                ;;
+                '5'*) echo ''
+                ;;
+                '4'*) echo ''
+                ;;
+                '3'*) echo ''
+                ;;
+                '2'*) echo ''
+                ;;
+                '1'*) echo ''
+                ;;
+                '0'*) echo ''
+                ;;
+                *) echo ''
+                ;;
+            esac
+        ;;
+        *) echo ''
+        ;;
+    esac
+
+}
+
+# RAM
+mem() {
+  mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "$mem"
+}
+
+# CPU
+cpu() {
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "$cpu%"
+}
+
+### }}}
+
+status(){
+	echo " $(capslock)      $(bat_per0) | $(bat_per1)      $(volume)      $(brightness)      $(network)      $(date +'%H:%M') "
+}
+
+
+while true; do
+	xsetroot -name "$(status)"
+	sleep 0.25
+done

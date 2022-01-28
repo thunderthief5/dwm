01c8df55c09be280a0662b76d4e4a04f1def325c
ThunderThief <thunderthief5@gmail.com>
2022-01-28 13:55:15 +0530

    Misc updates

--- config.h
+++ config.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 1;        /* vertical padding of bar */
-static const int sidepad            = 3;        /* horizontal padding of bar */
+static const int sidepad            = 5;        /* horizontal padding of bar */
 static const char *fonts[]          = { "Product Sans:style=Bold:size=11","Font Awesome 5 Free:style=Solid:size=11" };
 static const char dmenufont[]       = "Isonorm:size=14";
 static const char col_1[]           = "#1b1b1b";
@@ -53,11 +53,11 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[M-S]",    tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
- 	{ "[@]",      spiral },
- 	{ "[BSP]",    dwindle },
+	{ "T",      tile },    /* first entry is default */
+	{ "F",      NULL },    /* no layout function means floating behavior */
+	{ "M",      monocle },
+ 	{ "SP",     spiral },
+ 	{ "BSP",    dwindle },
 };
 
 /* key definitions */
